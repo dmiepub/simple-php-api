a1283aa90f4259a8c35b8ccd0d1f6087b4594990
dmiepub <dmiepub.dev@gmail.com>
2025-02-14 16:20:41 +0530

    Added COSR

--- public/index.php
+++ public/index.php
@@ -1,5 +1,14 @@
 <?php
+header("Access-Control-Allow-Origin: *"); // Allow all origins
+header("Access-Control-Allow-Methods: GET, POST, PUT, DELETE, OPTIONS");
+header("Access-Control-Allow-Headers: Content-Type, Authorization");
+header("Access-Control-Allow-Credentials: true");
 header("Content-Type: application/json");
+
+if ($_SERVER['REQUEST_METHOD'] === 'OPTIONS') {
+    http_response_code(200);
+    exit();
+}
 
 require_once "../config/database.php";
 
