aec467181dd93cc0e7e912fa4ac0f73ba194578a
dmiepub <dmiepub.dev@gmail.com>
2025-02-14 15:35:10 +0530

    new folder

---
+++ public/books/list.php
@@ -0,0 +1,10 @@
+<?php
+header("Content-Type: application/json");
+
+require_once "../../config/database.php";
+
+$query = $pdo->query("SELECT * FROM users");
+$users = $query->fetchAll(PDO::FETCH_ASSOC);
+
+echo json_encode(["status" => "success", "data" => $users]);
+?>
